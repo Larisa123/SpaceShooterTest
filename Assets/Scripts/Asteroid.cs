e3f77234b9223262f63a5b750b2d9c3e5ba1816a
Larisa <lara.carli@gmail.com>
2017-10-25 13:19:38 +0200

    update

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+	public float rotationSize, speed;
+
+	void Start ()
+	{
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.angularVelocity = Random.insideUnitSphere * rotationSize;
+
+		rb.velocity = transform.forward * speed;
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float tilt;
+	public int speed;
+	public float xMin, xMax; // boundary
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Rigidbody rb = GetComponent<Rigidbody> ();
+
+		rb.velocity = new Vector3 (moveHorizontal, 0.0f, moveVertical) * speed;
+
+		rb.position = new Vector3(
+			Mathf.Clamp(rb.position.x, xMin, xMax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, rb.position.z, rb.position.z)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+}
