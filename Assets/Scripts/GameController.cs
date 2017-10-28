26de6f7687e26fc30230886522dc731b098fe2a3
Larisa <lara.carli@gmail.com>
2017-10-28 09:24:46 +0200

    organised code

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject[] hazards;
-	public Vector3 spawnValues;
-	[SerializeField] private float spawnVariationZ;
-	[SerializeField] private float spawnVariationY;
-	public int hazardCount;
-	public GameObject hazard;
+	// Hazards:
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+
+	// ASTEROIDS:
+	public Vector3 spawnValues;
+	[SerializeField] private float spawnVariationZ;
+	[SerializeField] private float spawnVariationY;
+	public int asteroidCount;
+	public GameObject asteroid;
 
 	void Start () {
 
@@ -31,22 +33,29 @@ public class GameController : MonoBehaviour {
 	}
 	*/
 
+	// Asteroids:
+
 	IEnumerator SpawnWaves () {
 		yield return new WaitForSeconds (startWait);
 		while (true) {
-			for (int i = 0; i < hazardCount; i++)
+			for (int i = 0; i < asteroidCount; i++)
 			{
-				//GameObject hazard = hazards [Random.Range (0, hazards.Length)];
-				Vector3 spawnPosition = new Vector3 (
-					Random.Range (-spawnValues.x, spawnValues.x), 
-					Random.Range (spawnValues.y, spawnValues.y + spawnVariationY), 
-					Random.Range (spawnValues.z, spawnValues.z + spawnVariationZ)
-				);
-				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (hazard, spawnPosition, spawnRotation);
+				instantiateAsteroid ();
 				yield return new WaitForSeconds (spawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
+	}
+
+	// Bullets:
+
+	public void instantiateAsteroid() {
+		Vector3 spawnPosition = new Vector3 (
+			Random.Range (-spawnValues.x, spawnValues.x), 
+			Random.Range (spawnValues.y, spawnValues.y + spawnVariationY), 
+			Random.Range (spawnValues.z, spawnValues.z + spawnVariationZ)
+		);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (asteroid, spawnPosition, spawnRotation);
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,32 +3,42 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float tilt;
+	// Player - SHIP:
+	public float tilt; // when player moves
+	public float backThrust; // when player shoots a bullet
+	public float backThrustTime;
 	public int speed;
-	public float xMin, xMax; // boundary
-	public float yMin, yMax;
+	public float xMin, xMax; // player boundary
+	public float yMin, yMax; // player boundary
 
 
+	// BULLET:
 	public GameObject bullet;
 	public Transform bulletSpawn;
 	public float fireRate;
 
 	private float nextFire;
 
-	void Update (){
+	void Start() {
+	}
+
+	void Update () {
 		if (Time.time > nextFire ) {
-			nextFire = Time.time + fireRate;
-			Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
-			//GetComponent<AudioSource>().Play ();
+			shootBullet ();
 		}
 	}
 
 	void FixedUpdate () {
+		movePlayer ();
+	}
+
+	// Move player:
+
+	void movePlayer() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		Rigidbody rb = GetComponent<Rigidbody> ();
-
 		rb.velocity = new Vector3 (moveHorizontal, moveVertical, 0.0f) * speed;
 
 		rb.position = new Vector3(
@@ -39,4 +49,21 @@ public class PlayerController : MonoBehaviour {
 
 		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
 	}
+
+	IEnumerator thrustPlayerBack () {
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		rb.AddForce (-transform.forward * backThrust);
+		yield return new WaitForSeconds (backThrustTime);
+		rb.AddForce (transform.forward * backThrust);
+	}
+
+	// Bullets:
+
+	void shootBullet() {
+		nextFire = Time.time + fireRate;
+		Instantiate(bullet, bulletSpawn.position, bulletSpawn.rotation);
+		//GetComponent<AudioSource>().Play (); // sound effect
+		StartCoroutine(thrustPlayerBack());
+	}
+
 }
