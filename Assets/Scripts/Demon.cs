eb3b586909031cd338fd824068b514fb076ccdce
Larisa <lara.carli@gmail.com>
2017-11-01 10:59:12 +0100

    added magma demon!

---
+++ Assets/Scripts/Demon.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Demon : MonoBehaviour {
+
+	public Transform playerPos;
+
+	void Update () {
+		this.gameObject.transform.LookAt (playerPos);
+	}
+}
