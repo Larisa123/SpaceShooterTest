d897ee7194a7ad2f1274e9eabdab63a2613d0be3
Larisa <lara.carli@gmail.com>
2017-11-04 22:58:15 +0100

    demons move towards player, no pathfinding yet

--- Assets/_Scripts/Demon.cs
+++ Assets/_Scripts/Demon.cs
@@ -7,26 +7,40 @@ public class Demon : MonoBehaviour {
 	private Transform playerPos;
 	//public Transform spawnPos;
 
-	public GameObject fireAnimation;
+	public GameObject demonExplosion;
 	public GameObject bullet; // not really a bullet, but I will use this name for convention
 	public int demonSpeed;
 	public float fireRate;
 	//private float nextFire;
 	//public float fireRate;
 
-	private Animator animator;
+	//private Animator animator;
 
 	void Start() {
 		playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform> ();
-		animator = GetComponent<Animator> ();
+		//animator = GetComponent<Animator> ();
 		//animator.Play ("demonAttact");
 		//fireAnimation.Play ();
+		giveDemonVelocity();
 		StartCoroutine (shootBulletsCoroutine());
 	}
 
 
 	void Update () {
 		this.gameObject.transform.LookAt (playerPos);
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		GameObject collider = collision.collider.gameObject;
+		if (collider.tag == "PlayerBullet")
+			bulletHitDemon(collider);
+	}
+
+	void bulletHitDemon(GameObject bullet) {
+		Debug.Log("Demon Destroyed");
+		Instantiate (demonExplosion, this.transform.position, this.transform.rotation);
+		Destroy (bullet);
+		Destroy (this.gameObject);
 	}
 
 	void giveDemonVelocity() {
